298d724ff0c64c2c7c9c07464b0626f94615e4a3
Dave May <dave.mayhem23@gmail.com>
2020-07-30 12:59:49 +0100

    Rift3D with FV: For ease of testing, I introduce a new model just for FV. The model is intended to be identical to model rift3D_T.

---
+++ src/models/rift3D_TFV/rift3D_T_ctx.h
@@ -0,0 +1,52 @@
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ **
+ **    Copyright (c) 2012
+ **        Dave A. May [dave.may@erdw.ethz.ch]
+ **        Institute of Geophysics
+ **        ETH Zürich
+ **        Sonneggstrasse 5
+ **        CH-8092 Zürich
+ **        Switzerland
+ **
+ **    project:    pTatin3d
+ **    filename:   rift3D_T_ctx.h
+ **
+ **
+ **    pTatin3d is free software: you can redistribute it and/or modify
+ **    it under the terms of the GNU General Public License as published
+ **    by the Free Software Foundation, either version 3 of the License,
+ **    or (at your option) any later version.
+ **
+ **    pTatin3d is distributed in the hope that it will be useful,
+ **    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ **    See the GNU General Public License for more details.
+ **
+ **    You should have received a copy of the GNU General Public License
+ **    along with pTatin3d. If not, see <http://www.gnu.org/licenses/>.
+ **
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @*/
+
+#ifndef __ptatinmodel_rift3d_T_ctx_h__
+#define __ptatinmodel_rift3d_T_ctx_h__
+
+typedef struct {
+  PetscInt  nmaterials;
+  PetscBool runmises;
+  PetscReal Lx,Ly,Lz,Ox,Oy,Oz,vx,vy,vz,rho0;
+  PetscReal Tbottom,Ttop,thermal_age0;
+  PetscReal thermal_age_anom,wx_anom,wz_anom,cx_anom,cz_anom;
+  PetscBool dimensional;
+  PetscReal density_bar;
+  PetscReal length_bar;
+  PetscReal viscosity_bar;
+  PetscReal velocity_bar;
+  PetscReal time_bar;
+  PetscReal pressure_bar;
+  PetscBool use_semi_eulerian_mesh;
+  PetscBool output_markers;
+  GeometryObject G[100];
+  PetscInt  ngo;
+} ModelRift3D_TCtx;
+
+#endif
