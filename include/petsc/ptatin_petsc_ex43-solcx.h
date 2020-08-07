077653a9e0f0d76020b9d49c1df78ff3ac37182e
Dave May <dave.mayhem23@gmail.com>
2020-08-07 12:09:42 +0100

    petsc 3.13 update: Bump ex43 header

--- include/petsc/ptatin_petsc_ex43-solcx.h
+++ include/petsc/ptatin_petsc_ex43-solcx.h
@@ -4,10 +4,10 @@
 /* version check */
 #include "petscversion.h"
 
-#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR >= 9) && (PETSC_VERSION_MINOR <= 10))
+#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR >= 9) && (PETSC_VERSION_MINOR <= 13))
   #include "src/ksp/ksp/examples/tutorials/ex43-solcx.h"
 #else
-  #error "pTatin provided private petsc header for src/ksp/ksp/examples/tutorials/ex43-solcx.h is only valid for PETSc v3.9 or v3.10"
+  #error "pTatin provided private petsc header for src/ksp/ksp/examples/tutorials/ex43-solcx.h is only valid for PETSc v3.9, v3.10, v3.11, v3.12, v3.13"
 #endif
 
 #endif
