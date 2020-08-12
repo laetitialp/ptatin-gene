6c429f68d4740f77002dc8916bd196d1b29098aa
Dave May <dave.mayhem23@gmail.com>
2020-08-12 23:09:25 +0100

    FVDA private: Added missing prototype

--- src/finite_volume/fvda_private.h
+++ src/finite_volume/fvda_private.h
@@ -7,6 +7,7 @@ void EvaluateBasisDerivative_Q1_1D(const PetscReal xi[], PetscReal dN[][DACELL1D
 void EvaluateBasis_Q1_2D(const PetscReal xi[], PetscReal N[]);
 void EvaluateBasisDerivative_Q1_2D(const PetscReal xi[], PetscReal dN[][DACELL2D_Q1_SIZE]);
 void EvaluateBasis_Q1_3D(const PetscReal xi[], PetscReal N[]);
+void EvaluateBasisDerivative_Q1_3D(const PetscReal xi[], PetscReal dN[][DACELL3D_Q1_SIZE]);
 
 PetscErrorCode DACellGeometry2d_GetFaceIndices(DM dm,DACellFace face,PetscInt fidx[]);
 PetscErrorCode DACellGeometry3d_GetFaceIndices(DM dm,DACellFace face,PetscInt fidx[]);
