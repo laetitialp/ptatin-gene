db15d9484bd2d815f7aab07279661c1650fb370d
Anthony_Jourdon <jourdon_anthony@hotmail.fr>
2021-02-09 07:18:06 +0100

    Added vdot computing and using vdotn to determine on which face impose the BC from cell value

--- src/finite_volume/fv-ex10a.c
+++ src/finite_volume/fv-ex10a.c
@@ -53,6 +53,56 @@ static PetscErrorCode FVSetDirichletFromNeighbour(FVDA fv,Vec T,DACellFace face)
   PetscFunctionReturn(0);
 }
 
+static PetscErrorCode FVSetDirichletFromNeighbourVdotN(FVDA fv,Vec T,DACellFace face)
+{
+  PetscInt       f,len,s,e;
+  const PetscInt *indices;
+  const PetscReal *vdotn;
+  PetscInt       cell;
+  Vec            Tl;
+  DM             dm;
+  PetscScalar    *LA_T;
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  // Get rank-local T values
+  dm = fv->dm_fv;
+
+  ierr = DMGetLocalVector(dm,&Tl);CHKERRQ(ierr);
+  ierr = DMGlobalToLocal(dm,T,INSERT_VALUES,Tl);CHKERRQ(ierr);
+  ierr = VecGetArray(Tl,&LA_T);CHKERRQ(ierr);
+
+  //ierr = VecGetArray(T,&LA_T);CHKERRQ(ierr);
+  // Get boundary cells faces indices
+  ierr = FVDAGetBoundaryFaceIndicesRead(fv,face,&len,&indices);CHKERRQ(ierr);
+  ierr = FVDAGetBoundaryFaceIndicesOwnershipRange(fv,face,&s,&e);CHKERRQ(ierr);
+
+  ierr = FVDAGetFacePropertyByNameArrayRead(fv,"v.n",&vdotn);CHKERRQ(ierr);
+  
+  for (f=0; f<len; f++) {
+    PetscInt fvid = indices[f];
+    cell = fv->face_element_map[2*fvid + 0];
+	if (vdotn[fvid] < 0.0){
+      fv->boundary_value[s + f] = LA_T[cell];
+      fv->boundary_flux[s + f] = FVFLUX_DIRICHLET_CONSTRAINT;
+	  //PetscPrintf(PETSC_COMM_SELF,"fid=indices[%d]=%d, cell[%d]=face_element_map[%d]=%d \n",f,indices[f],f,2*fvid + 0,cell);
+    } else {
+      fv->boundary_value[s + f] = 0.3;
+      fv->boundary_flux[s + f] = FVFLUX_DIRICHLET_CONSTRAINT;
+    }
+	/* This may not be required in solved problems since the BC insertions are embeded inside the matrix operations */
+    //ierr = VecSetValue(T,cell,fv->boundary_value[s + f],INSERT_VALUES);CHKERRQ(ierr);
+	ierr = VecSetValue(Tl,cell,fv->boundary_value[s + f],INSERT_VALUES);CHKERRQ(ierr);
+  }
+  ierr = DMLocalToGlobal(dm,Tl,INSERT_VALUES,T);CHKERRQ(ierr);
+  ierr = VecAssemblyBegin(Tl);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(Tl);CHKERRQ(ierr);  
+  ierr = VecRestoreArray(Tl,&LA_T);CHKERRQ(ierr);
+  ierr = DMRestoreLocalVector(dm,&Tl);CHKERRQ(ierr);
+
+  PetscFunctionReturn(0);
+}
+
 static PetscErrorCode PrintBoundaryValues(FVDA fv,Vec T,DACellFace face)
 {
   PetscInt       f,len,s,e;
@@ -126,6 +176,22 @@ PetscErrorCode t10a(void)
     }
   }
   
+  ierr = FVDARegisterFaceProperty(fv,"v.n",1);CHKERRQ(ierr);
+  {
+    PetscInt        f,nfaces;
+    const PetscReal *face_centroid,*face_normal;
+    PetscReal       *vdotn;
+    const PetscReal velocity[] = { 1.0e1, 1.0e1, 0.0 }; /* imposed velocity field */
+    
+    ierr = FVDAGetFaceInfo(fv,&nfaces,NULL,NULL,&face_normal,&face_centroid);CHKERRQ(ierr);
+    ierr = FVDAGetFacePropertyArray(fv,1,&vdotn);CHKERRQ(ierr);
+    for (f=0; f<nfaces; f++) {
+      vdotn[f] = velocity[0] * face_normal[3*f+0]
+      + velocity[1] * face_normal[3*f+1]
+      + velocity[2] * face_normal[3*f+2];
+    }
+  }
+  
   dm = fv->dm_fv;
   ierr = DMCreateGlobalVector(dm,&T);CHKERRQ(ierr);
   ierr = FVDAVecTraverse(fv,T,0.0,0,iterator_initial_thermal_field,NULL);CHKERRQ(ierr);
@@ -134,7 +200,8 @@ PetscErrorCode t10a(void)
   const DACellFace flist[] = { DACELL_FACE_W, DACELL_FACE_E, DACELL_FACE_S, DACELL_FACE_N, DACELL_FACE_B, DACELL_FACE_F };
   PetscInt l;
   for (l=0; l<sizeof(flist)/sizeof(DACellFace); l++) {
-    ierr = FVSetDirichletFromNeighbour(fv,T,flist[l]);CHKERRQ(ierr);
+    //ierr = FVSetDirichletFromNeighbour(fv,T,flist[l]);CHKERRQ(ierr);
+	ierr = FVSetDirichletFromNeighbourVdotN(fv,T,flist[l]);CHKERRQ(ierr);
 	//ierr = PrintBoundaryValues(fv,T,flist[l]);CHKERRQ(ierr);
   }
   VecAssemblyBegin(T);
