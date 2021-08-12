2db735ef2008a7a8b29b94b2fa79ba652b430b1e
Dave May <dave.mayhem23@gmail.com>
2021-08-12 13:59:34 +0200

    Enhancement: New coarse grid operators for nonlinear problems. * MFGalerkin: Defines a Galerkin coarse grid without assembling the fine grid. Index type = 3. Example: -A11_operator_type 2,3,1 * SNESFD: Assembles the Jacobian associated with only Juu. Only valid on the finest level. Index type = 4. Example: -A11_operator_type 2,2,4

--- src/stokes_form_function.c
+++ src/stokes_form_function.c
@@ -1534,3 +1534,86 @@ PetscErrorCode patch_MatMFFDSetBase_MFFD(Mat J,Vec U,Vec F)
   J->assembled = PETSC_TRUE;
   return(0);
 }
+
+PetscErrorCode FormFunction_StokesU(SNES snes,Vec X,Vec F,void *ctx)
+{
+  PetscErrorCode    ierr;
+  pTatinCtx         ptatin;
+  DM                stokes_pack,dau,dap;
+  Vec               Uloc,Ploc,FUloc,FPloc;
+  Vec               u,p,Fu,Fp;
+  PetscScalar       *LA_Uloc,*LA_Ploc;
+  PetscScalar       *LA_FUloc,*LA_FPloc;
+  PhysCompStokes    stokes;
+  
+  PetscFunctionBegin;
+  
+  ptatin      = (pTatinCtx)ctx;
+  //  stokes      = ptatin->stokes_ctx;
+  ierr = pTatinGetStokesContext(ptatin,&stokes);CHKERRQ(ierr);
+  stokes_pack = stokes->stokes_pack;
+  
+  ierr = DMCompositeGetEntries(stokes_pack,&dau,&dap);CHKERRQ(ierr);
+  
+  ierr = DMCompositeGetLocalVectors(stokes_pack,&Uloc,&Ploc);CHKERRQ(ierr);
+  ierr = DMCompositeGetLocalVectors(stokes_pack,&FUloc,&FPloc);CHKERRQ(ierr);
+  
+  /* get the local (ghosted) entries for each physics */
+  //ierr = DMCompositeScatter(stokes_pack,X,Uloc,Ploc);CHKERRQ(ierr);
+  ierr = DMGlobalToLocalBegin(dau,X,INSERT_VALUES,Uloc);CHKERRQ(ierr);
+  ierr = DMGlobalToLocalEnd  (dau,X,INSERT_VALUES,Uloc);CHKERRQ(ierr);
+  ierr = VecZeroEntries(Ploc);CHKERRQ(ierr);
+  
+  /* insert boundary conditions into local vectors */
+  ierr = BCListInsertLocal(stokes->u_bclist,Uloc);CHKERRQ(ierr);
+  
+  ierr = VecGetArray(Uloc,&LA_Uloc);CHKERRQ(ierr);
+  ierr = VecGetArray(Ploc,&LA_Ploc);CHKERRQ(ierr);
+  
+  /* compute Ax - b */
+  ierr = VecZeroEntries(FUloc);CHKERRQ(ierr);
+  ierr = VecZeroEntries(FPloc);CHKERRQ(ierr);
+  ierr = VecGetArray(FUloc,&LA_FUloc);CHKERRQ(ierr);
+  ierr = VecGetArray(FPloc,&LA_FPloc);CHKERRQ(ierr);
+  
+  /* ======================================== */
+  /*         UPDATE NON-LINEARITIES           */
+  /* evaluate rheology and rhs using X        */
+  /* map marker eta to quadrature points */
+  /* map marker force to quadrature points */
+  /* ======================================== */
+  ierr = pTatin_EvaluateRheologyNonlinearities(ptatin,dau,LA_Uloc,dap,LA_Ploc);CHKERRQ(ierr);
+  
+  /* Form scaling for momentum */
+  //ierr = FormScaling_U_etaMassMatrixDiagonal(user,dau,user->u_bclist);CHKERRQ(ierr);
+  
+  /* momentum */
+  ierr = FormFunctionLocal_U(stokes,dau,LA_Uloc,dap,LA_Ploc,LA_FUloc);CHKERRQ(ierr);
+  //ierr = FormFunctionLocal_U_tractionBC(stokes,dau,LA_Uloc,dap,LA_Ploc,LA_FUloc);CHKERRQ(ierr);
+  
+  ierr = VecRestoreArray(FPloc,&LA_FPloc);CHKERRQ(ierr);
+  ierr = VecRestoreArray(FUloc,&LA_FUloc);CHKERRQ(ierr);
+  ierr = VecRestoreArray(Ploc,&LA_Ploc);CHKERRQ(ierr);
+  ierr = VecRestoreArray(Uloc,&LA_Uloc);CHKERRQ(ierr);
+  
+  /* do global fem summation */
+  ierr = VecZeroEntries(F);CHKERRQ(ierr);
+  //ierr = DMCompositeGather(stokes_pack,ADD_VALUES,F,FUloc,FPloc);CHKERRQ(ierr);
+  ierr = DMLocalToGlobalBegin(dau,FUloc,ADD_VALUES,F);CHKERRQ(ierr);
+  ierr = DMLocalToGlobalEnd  (dau,FUloc,ADD_VALUES,F);CHKERRQ(ierr);
+
+  ierr = DMCompositeRestoreLocalVectors(stokes_pack,&FUloc,&FPloc);CHKERRQ(ierr);
+  ierr = DMCompositeRestoreLocalVectors(stokes_pack,&Uloc,&Ploc);CHKERRQ(ierr);
+  
+  /* modify F for the boundary conditions, F_k = scale_k(x_k - phi_k) */
+  //ierr = DMCompositeGetAccess(stokes_pack,F,&Fu,&Fp);CHKERRQ(ierr);
+  //ierr = DMCompositeGetAccess(stokes_pack,X,&u,&p);CHKERRQ(ierr);
+  
+  //ierr = BCListResidualDirichlet(stokes->u_bclist,u,Fu);CHKERRQ(ierr);
+  ierr = BCListResidualDirichlet(stokes->u_bclist,X,F);CHKERRQ(ierr);
+  
+  //ierr = DMCompositeRestoreAccess(stokes_pack,X,&u,&p);CHKERRQ(ierr);
+  //ierr = DMCompositeRestoreAccess(stokes_pack,F,&Fu,&Fp);CHKERRQ(ierr);
+  
+  PetscFunctionReturn(0);
+}
