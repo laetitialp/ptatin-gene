0a3cb7a8b10423d96ba4b29485a53652e18a4faa
Anthony_Jourdon <jourdon_anthony@hotmail.fr>
2021-10-29 09:48:20 +0200

    Moved the marker injection after the population control so the marker projection on gauss points, the velocity solve and the output are done using the newly injected markers. I prefer this way, because the population control removes almost all of the newly inserted markers, but I do want that the newly inserted markers are accounted for the solve and the output

--- src/tests/test_driver_checkpoint_fv.c
+++ src/tests/test_driver_checkpoint_fv.c
@@ -2887,11 +2887,11 @@ PetscErrorCode Run_NonLinearFV(pTatinCtx user,Vec v1,Vec v2)
     /* 3 Update local coordinates and communicate */
     ierr = MaterialPointStd_UpdateCoordinates(user->materialpoint_db,dav,user->materialpoint_ex);CHKERRQ(ierr);
 
-    /* 3a - Add material */
-    ierr = pTatinModel_ApplyMaterialBoundaryCondition(model,user);CHKERRQ(ierr);
-
     /* add / remove points if cells are over populated or depleted of points */
     ierr = MaterialPointPopulationControl_v1(user);CHKERRQ(ierr);
+
+    /* 3a - Add material */
+    ierr = pTatinModel_ApplyMaterialBoundaryCondition(model,user);CHKERRQ(ierr);
 
     /* update markers = >> gauss points */
     {
