68d2e00392991e92ab0227f703f6619ad3050264
Anthony_Jourdon <jourdon_anthony@hotmail.fr>
2022-04-30 10:38:54 +0200

    Passed NULL to the last argument of the function MatA11MFSetup() as SurfaceQuadrature was added to the argument because we need it for the Nitsche method

--- src/ptatin_driver_ts_init.c
+++ src/ptatin_driver_ts_init.c
@@ -281,7 +281,7 @@ PetscErrorCode pTatin3dCreateStokesOperators(PhysCompStokes stokes_ctx,IS is_sto
 
         if (!been_here) PetscPrintf(PETSC_COMM_WORLD,"Level [%D]: Coarse grid type :: Re-discretisation :: matrix free operator \n",k);
         ierr = MatA11MFCreate(&A11Ctx);CHKERRQ(ierr);
-        ierr = MatA11MFSetup(A11Ctx,dav_hierarchy[k],volQ[k],u_bclist[k]);CHKERRQ(ierr);
+        ierr = MatA11MFSetup(A11Ctx,dav_hierarchy[k],volQ[k],u_bclist[k],NULL);CHKERRQ(ierr);
 
         ierr = StokesQ2P1CreateMatrix_MFOperator_A11(A11Ctx,&Auu);CHKERRQ(ierr);
         /* memory saving - only need daU IF you want to split A11 into A11uu,A11vv,A11ww */
