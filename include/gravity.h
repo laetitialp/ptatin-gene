9c449eeabe6f48087bd816d0470c0f76f1d7cc6d
anthony_jourdon <jourdon_anthony@hotmail.fr>
2023-05-31 11:54:49 +0200

    Removed the data structures of the different gravity types in favour of their own files. Added functions pointers for methods that are common to every gravity types. The function corresponding to the gravity type is attributed to the function pointer in the function creating and allocating the data structure for the selected type. The data structure of the gravity type can be recovered by typecasting the void *data pointer into the corresponding data structure type. For now there are function pointers to: - destroy (free the data structures related to gravity) - scale (scaling of the passed arguments to the data structure of the gravity type that need scaling) - set (set the parameters defined in the data structure of the gravity type, uses a void* for flexibility regarding the data structures contents) - quadrature_set (The function setting the gravity and body forces on the quadrature points) - update (The function updating the gravity and body forces on the quadrature points) Except if more function pointers are needed, the GravityModel data structure containing them should not need modification when adding a new type (only the enum GravityType).

--- include/gravity.h
+++ include/gravity.h
@@ -1,6 +1,9 @@
 #ifndef __ptatin_gravity_h__
 #define __ptatin_gravity_h__
 
+#include "gravity/gravity_constant.h"
+#include "gravity/gravity_radialconstant.h"
+
 typedef enum {
   CONSTANT=0,
   RADIAL_CONSTANT,
@@ -9,51 +12,27 @@ typedef enum {
   POISSON
 } GravityType;
 
-typedef struct _p_GravityConstant *GravityConstant;
-typedef struct _p_GravityRadialConst *GravityRadialConst;
-
 struct _p_GravityModel {
-  GravityType gravity_type; 
-  void        *data;
+  GravityType gravity_type;
+  void        *data; /* Gravity type data structure */
+  PetscErrorCode (*destroy)(GravityModel);
+  PetscErrorCode (*scale)(GravityModel,PetscReal);
+  PetscErrorCode (*set)(GravityModel,void*);
+  PetscErrorCode (*quadrature_set)(PhysCompStokes,GravityModel);
+  PetscErrorCode (*update)(PhysCompStokes,GravityModel);
 };
 
-struct _p_GravityConstant {
-  PetscReal gravity_const[3]; /* Constant gravity vector */
-  PetscReal magnitude;        /* Magnitude of the gravity vector */
-  PetscReal scaling_factor;
-};
-
-
-struct _p_GravityRadialConst {
-  PetscReal gravity_mag;
-  PetscReal scaling_factor;
-};
-
-
-PetscErrorCode GravityCreateCtx(GravityModel gravity);
-PetscErrorCode pTatinCreateGravityModel(pTatinCtx ptatin, GravityType gtype, void *data);
-PetscErrorCode GravityGetConstantCtx(GravityModel gravity, GravityConstant *ctx);
-/*
-PetscErrorCode GravityModelDestroyCtx(GravityModel *gravity);
-PetscErrorCode pTatinGetContext_GravityModel(pTatinCtx ptatin, GravityModel *gravity);
-
+PetscErrorCode GravityModelCreateCtx(GravityModel *gravity);
 PetscErrorCode GravitySetType(GravityModel gravity, GravityType gtype);
-PetscErrorCode GravitySet_GravityConst(GravityModel gravity, PetscReal gvec[]);
-PetscErrorCode GravitySet_GravityMag(GravityModel gravity, PetscReal magnitude);
-PetscErrorCode GravitySetValues_ConstantVector(GravityModel gravity, void *data);
-PetscErrorCode GravitySetValues_Magnitude(GravityModel gravity, void *data);
-PetscErrorCode GravitySetValues(GravityModel gravity, void *data);
+PetscErrorCode GravityScale(GravityModel gravity, PetscReal scaling_factor);
+PetscErrorCode GravitySet(GravityModel gravity, void *data);
+PetscErrorCode GravityModelDestroyCtx(GravityModel *gravity);
+PetscErrorCode pTatinDestroyGravityModelCtx(pTatinCtx ptatin);
+PetscErrorCode pTatinGetGravityModelCtx(pTatinCtx ptatin, GravityModel *ctx);
+PetscErrorCode GravityCreateTypeCtx(GravityModel gravity);
+PetscErrorCode pTatinCreateGravityModel(pTatinCtx ptatin, GravityType gtype);
+PetscErrorCode QuadratureSetBodyForcesOnPoint(QPntVolCoefStokes *cell_gausspoints, PetscInt qp_idx);
+PetscErrorCode pTatinQuadratureSetGravityModel(pTatinCtx ptatin);
+PetscErrorCode pTatinQuadratureUpdateGravityModel(pTatinCtx ptatin);
 
-PetscErrorCode GravityScale_GravityConst(GravityModel gravity, PetscReal fac);
-PetscErrorCode GravityScale_GravityMag(GravityModel gravity, PetscReal fac);
-PetscErrorCode GravityScale(GravityModel gravity, PetscReal fac);
-PetscErrorCode pTatinCreateGravityModel(pTatinCtx ptatin, GravityType gtype, PetscReal scaling_factor, void *data);
-
-PetscErrorCode QuadratureSetGravityModel(PhysCompStokes stokes, QPntVolCoefStokes *all_gausspoints, GravityModel gravity, PetscBool body_forces);
-PetscErrorCode GravityModelUpdateQuadraturePoints(PhysCompStokes stokes, QPntVolCoefStokes *all_gausspoints, GravityModel gravity, PetscBool body_forces);
-PetscErrorCode pTatin_ApplyInitialStokesBodyForcesModel(pTatinCtx ptatin);
-PetscErrorCode pTatin_ApplyInitialStokesGravityModel(pTatinCtx ptatin);
-PetscErrorCode pTatin_UpdateStokesBodyForcesModel(pTatinCtx ptatin);
-PetscErrorCode pTatin_UpdateStokesGravityModel(pTatinCtx ptatin);
-*/
 #endif
